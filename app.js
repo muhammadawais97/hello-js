484bd89e57ff48c484963f2a431c3d6a1e7c36dc
muhammad awais <154671272+muhammadawais97@users.noreply.github.com>
2024-02-27 18:42:50 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,45 @@
+///////////////////////////////////////////////////////chapter 01 start///////////////////////////////////////////////////////////////////////////////////////////
+
+//alert("hello js");
+//alert("Hello... I can run JS through my web browser's console");
+//alert("Error! Please enter a valid password.");
+//alert("Welcome to JS land... \nHappyy Coding!");
+//alert("Happy Coding!");
+
+///////////////////////////////////////////////////////chapter-1 end////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+/////////////////////////////////////////////////////chapter-2 start////////////////////////////////////////////////////////////////////////////////////////////
+//var username;
+//var myName = "muhammad awais";
+//alert ("muhammad awais");
+
+//var message;
+//message = "Hello World";
+//alert(message);
+
+//var studentName = "muhammad awais";
+//var studentAge = 25;
+//var studentGrade = "A";
+//alert("Student Name: " + studentName + "\nAge: " + studentAge + "\nGrade: " + studentGrade);
+
+//var alertMessage = "This is a specific alert message using one JS variable.";
+//alert(alertMessage);
+
+//var email = "memonawais@gmail.com";
+//alert("My email address is: " + email);
+
+//var book = "A smarter way to learn JavaScript";
+//alert("I am reading the book titled: '" + book + "'");
+
+//document.write("This content is displayed in the browser through JavaScript.");
+//var specialString = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+//alert(specialString);
+//document.write(specialString);
+
+//////////////////////////////////////////////////chapter-2 end////////////////////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////chapter-3 start----------------------------------------------------------------------------------------
+
+
